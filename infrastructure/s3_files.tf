c437e7e82481df3e3bc358e0c812990847281b61
Luiz Teixeira Lima Teixeira Junior <luiz.teixeira@becomex.com.br>
2022-03-16 14:28:38 -0300

    exclusão arquivos do s3_files

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -6,18 +6,10 @@ resource "aws_s3_bucket_object" "rais_ingestion" {
   etag   = filemd5("../etl/01_rais_ingestion.py")
 }
 
-resource "aws_s3_bucket_object" "rais_transformation" {
-  bucket = aws_s3_bucket.dl.id
-  key    = "emr-code/pyspark/02_rais_transformation.py"
-  acl    = "private"
-  source = "../etl/02_rais_transformation.py"
-  etag   = filemd5("../etl/02_rais_transformation.py")
-}
-
 resource "aws_s3_bucket_object" "build_lambda_package" {
   bucket = aws_s3_bucket.dl.id
-  key    = "emr-code/pyspark/build_lambda_package.sh"
+  key    = "emr-code/build_lambda_package.sh"
   acl    = "private"
   source = "../scripts/build_lambda_package.sh"
-  etag   = filemd5("../etl/build_lambda_package.sh")
+  etag   = filemd5("../scripts/build_lambda_package.sh")
 }
